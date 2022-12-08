dca95496d856957442ae32a3766415188b5a2046
JonMS95 <jon_portugalete@hotmail.com>
2022-12-09 00:34:40 +0100

    Learning how to correctly measure errors in a model. It is going to be executed in a fair way this time. Work in progress.

---
+++ 5_Model_Validation/main.py
@@ -0,0 +1,27 @@
+import pandas as pd
+from constants import *
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.metrics import mean_absolute_error
+
+melbourne_data = pd.read_csv(PATH_MELBOURNE_CSV)
+
+melbourne_data_cols = melbourne_data.columns
+
+melbourne_data = melbourne_data.dropna(axis = 0)
+
+y = melbourne_data.Price
+
+melbourne_features = ['Rooms', 'Bathroom', 'Landsize', 'BuildingArea', 'YearBuilt', 'Lattitude', 'Longitude']
+X = melbourne_data[melbourne_features]
+
+X_data_frame = X.describe()
+
+X_first_five_rows = X.head()
+
+melbourne_model = DecisionTreeRegressor(random_state = DTR_RANDOM_STATE)
+melbourne_model.fit(X, y)
+
+melbourne_predict_prices = melbourne_model.predict(X)
+melbourne_MAE = mean_absolute_error(y, melbourne_predict_prices)
+
+print(melbourne_MAE)
