e75d4381a71440e3d0784afb09044a4966570369
JonMS95 <jon_portugalete@hotmail.com>
2022-12-09 18:00:05 +0100

    Renamed directories, just that.

--- 3_First_Coding_Exercise/constants.py
+++
@@ -1,1 +0,0 @@
-PATH_IOWA_CSV   = "/home/jon/Desktop/scripts/ML/Intro_to_Machine_Learning/Data_sets/iowa_data.csv"
--- 3_First_Coding_Exercise/main.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-from constants import *
-
-iowa_data = pd.read_csv(PATH_IOWA_CSV)
-
-iowa_data_frame = iowa_data.describe()
