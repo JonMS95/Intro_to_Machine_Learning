fb46b7ce55fe56bd37889baa11e1d17aba0ada74
JonMS95 <jon_portugalete@hotmail.com>
2022-12-09 01:35:21 +0100

    Removed unnecessary constants, added useful ones.

--- 5_Model_Validation/constants.py
+++ 5_Model_Validation/constants.py
@@ -1,12 +1,8 @@
 PATH_MELBOURNE_CSV  = "/home/jon/Desktop/scripts/ML/Intro_to_Machine_Learning/Data_sets/melb_data.csv"
 
-TEXT_SEPARATOR          = "*" * 80
-TEXT_COLS               = "Columns in the melbourne data CSV file."
-TEXT_FEATURES           = "Features we are going to work with."
-TEXT_FEAT_FIVE_ROWS     = "Data stored within the first five rows of the features variable (X)."
-TEXT_PREDICTIONS        = "The predictions are: "
-TEXT_FIVE_TARGET_PRICES = "The actual prices for the first five provided houses are:"
-TEXT_FIVE_PRED_PRICES   = "The predicted prices for the first five provided houses are:"
+TEXT_SEPARATOR          = "*" * 100
+TEXT_FIRST_MAE          = "The MAE for the model that is trained and validated by using the same data is: \t"
+TEXT_FAIR_MAE           = "The MAE for the model that is fairly trained and validated after is: \t\t"
 
 DTR_RANDOM_STATE    = 1
 SPLIT_RANDOM_STATE  = 0
