e232194b48b8238b84cc00d17a921f7cceaf677e
JonMS95 <jon_portugalete@hotmail.com>
2022-12-09 01:27:48 +0100

    Imported train_test_split function. Created a fair model for the Melbourne data, by using only training data. Validated it by using only validation data.

--- 5_Model_Validation/main.py
+++ 5_Model_Validation/main.py
@@ -4,6 +4,8 @@ from sklearn.tree import DecisionTreeRegressor
 
 # Apart from the ones above, some functions have to be imported additionally in order to calculate the mean absolute error (MAE) later.
 from sklearn.metrics import mean_absolute_error
+# Additionally, a function that splits the data in different intervals (for trainig and validation) is imprted as well.
+from sklearn.model_selection import train_test_split
 
 # The steps taht are followed are pretty much the same as in the last lesson except for the ones after the prediction. Let's enumerate them below:
 #     1-Get the data from the CSV file.
@@ -41,3 +43,14 @@ print(melbourne_MAE)
 
 # Until this point, it can be noticed that mean absolute error is unsurprisingly low. It's something expectable, as the data that was used first to train the model
 # is the same as the data that's being used later to test it, which is quite unfair, to be honest.
+# Training and validation data is going to be split into two parts, for both X (features) and y (target). 'train_test_split function' is being used for this purpose.
+train_X, val_X, train_y, val_y = train_test_split(X, y, random_state = SPLIT_RANDOM_STATE)
+
+# Now, a DecisionTreeRegressor object is going to be generated, and after that, it's going to be fitted by using only the training data (train_X, train_y).
+melbourne_fair_model = DecisionTreeRegressor(random_state = DTR_RANDOM_STATE)
+melbourne_fair_model.fit(train_X, train_y)
+
+# Let's calculate and display the MAE now. To do so, generate the predictions first, then get the MAE. Only validation data is used in this process.
+melbourne_fair_prediction = melbourne_fair_model.predict(val_X)
+melbourne_fair_MAE = mean_absolute_error(y, melbourne_fair_prediction)
+print(melbourne_fair_MAE)
