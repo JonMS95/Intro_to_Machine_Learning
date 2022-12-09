f74da72b815e8a4d580f1a412d81cf4a5ea47692
JonMS95 <jon_portugalete@hotmail.com>
2022-12-09 17:46:32 +0100

    Added some constants that are necessary for this exercise.

---
+++ 6_Underfitting_and_Overfitting/constants.py
@@ -0,0 +1,8 @@
+PATH_MELBOURNE_CSV  = "/home/jon/Desktop/scripts/ML/Intro_to_Machine_Learning/Data_sets/melb_data.csv"
+
+TEXT_SEPARATOR  = "*" * 50
+
+DTR_RANDOM_STATE    = 0
+SPLIT_RANDOM_STATE  = 0
+
+MAX_LEAF_NODES = [5, 50, 500, 5000]
